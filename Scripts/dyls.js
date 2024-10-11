292ef311c289d48b80e9b865e1f2c76e2954f6b3
mujinbo <316216012@qq.com>
2024-10-11 12:13:16 +0800

    Update dyls.js

--- Scripts/dyls.js
+++ Scripts/dyls.js
@@ -1,17 +1,15 @@
 /**********************************************
 > 应用名称：电影猎手
-> 特别提醒：电影猎手Library文件夹禁止写入权限
+> 特别提醒：Library文件夹禁Write权限
 > 脚本说明：去除首页轮播图广告、首页信息流广告、我的页面推广、缩短开屏广告倒计时
+> 更新时间：2024-10-11
 
 [rewrite_local]
-
 ^https?:\/\/[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+){1,3}(:\d+)?\/api\/v\d\/movie\/index_recommend url script-response-body https://raw.githubusercontent.com/neishe321/QuanX/main/Scripts/dyls.js
 ^https?:\/\/[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+){1,3}(:\d+)?\/api\/v\d\/advert url reject-200
 
 [mitm]
-
 hostname = app-v1.ecoliving168.com
-
 **********************************************/
 const url = $request.url;
 let obj = JSON.parse($response.body);
