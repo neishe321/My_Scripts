45c569ad2862edb85e837a51ee7832cb0b2f7f95
mujinbo <316216012@qq.com>
2024-10-13 21:50:00 +0800

    Update weibo_intl.js

--- Scripts/weibo_intl.js
+++ Scripts/weibo_intl.js
@@ -2,11 +2,11 @@
 // 关系映射
 const otherUrls = {
     // "php?a=search_topic": removeSearchTopic // 搜索话题
+    // "interface/sdk/sdkad.php": removePhpScreenAds // SDK
     "php?a=user_center": modifiedUserCenter, // 用户中心
     "php?a=open_app": removeAdBanner,  // 帖子广告banner
     "php?a=trends": removeTopics,          // 趋势页card
-    "php?a=get_coopen_ads": removeIntlOpenAds, // 开屏广告
-    "interface/sdk/sdkad.php": removePhpScreenAds // SDK广告  
+    "php?a=get_coopen_ads": removeIntlOpenAds // 开屏广告
 };
 
 // 获取处理函数
@@ -41,7 +41,6 @@ function modifiedUserCenter(e) {
     return e;
 }
 
-
 function removeTopics(e) {
     if (e.data) {
         e.data.order = ["discover", "search_topic"];
@@ -59,25 +58,25 @@ function removeIntlOpenAds(e) {
     return e;
 }
 
-function removePhpScreenAds(e) {
-    if (!e.ads) return e;
-    e.show_push_splash_ad = false;
-    e.background_delay_display_time = 0;
-    e.lastAdShow_delay_display_time = 0;
-    e.realtime_ad_video_stall_time = 0;
-    e.realtime_ad_timeout_duration = 0;
+// function removePhpScreenAds(e) {
+//     if (!e.ads) return e;
+//     e.show_push_splash_ad = false;
+//     e.background_delay_display_time = 0;
+//     e.lastAdShow_delay_display_time = 0;
+//     e.realtime_ad_video_stall_time = 0;
+//     e.realtime_ad_timeout_duration = 0;
 
-    for (let t of e.ads) {
-        t.displaytime = 0;
-        t.displayintervel = 86400;
-        t.allowdaydisplaynum = 0;
-        t.displaynum = 0;
-        t.displaytime = 1;
-        t.begintime = "2029-12-30 00:00:00";
-        t.endtime = "2029-12-30 23:59:59";
-    }
-    return e;
-}
+//     for (let t of e.ads) {
+//         t.displaytime = 0;
+//         t.displayintervel = 86400;
+//         t.allowdaydisplaynum = 0;
+//         t.displaynum = 0;
+//         t.displaytime = 1;
+//         t.begintime = "2029-12-30 00:00:00";
+//         t.endtime = "2029-12-30 23:59:59";
+//     }
+//     return e;
+// }
 
 // function removeSearchTopic(e) {
 //     if (e.data && e.data.search_topic && e.data.search_topic.cards.length > 0) {
@@ -98,8 +97,8 @@ if (modifyFunction) {
     let data = JSON.parse(body.match(/\{.*\}/)[0]); 
     modifyFunction(data); 
     body = JSON.stringify(data); 
-    if (modifyFunction === removePhpScreenAds) {
-        body = JSON.stringify(data) + "OK";
+    // if (modifyFunction === removePhpScreenAds) {
+    //     body = JSON.stringify(data) + "OK";
     }
 }
 
