e23c302c8cd7ee413c36f0167f244758d6968b2f
mujinbo <316216012@qq.com>
2024-10-29 14:42:40 +0800

    Update weibo_intl.js

--- Scripts/weibo_intl.js
+++ Scripts/weibo_intl.js
@@ -1,80 +1,58 @@
 // 脚本自定义优化于：https://raw.githubusercontent.com/ddgksf2013/Scripts/master/weibo_json.js
 
-const otherUrls = {
-    // "php?a=search_topic": removeSearchTopic, // 搜索话题
-    "php?a=user_center": modifiedUserCenter, // 用户中心
-    "php?a=open_app": removeAdBanner,  // 帖子下方广告banner
-    "php?a=trends": removeTopics,          // 趋势页card
-    "php?a=get_coopen_ads": removeIntlOpenAds // 开屏广告
+const modifyHandlers = {
+    "php?a=user_center": modifyUserCenter,      // 用户中心
+    "php?a=open_app": removeAdBanner,           // 帖子下方广告 banner
+    "php?a=trends": removeTrendsCards,          // 趋势页卡片
+    "php?a=get_coopen_ads": removeOpenAds       // 开屏广告
 };
 
-function getModifyMethod(url) {
-    for (let key in otherUrls) {
-        if (url.includes(key)) {
-            return otherUrls[key]; 
-        }
-    }
-    return null; 
+function getModifyHandler(url) {
+    return Object.keys(modifyHandlers).find(key => url.includes(key)) ? modifyHandlers[Object.keys(modifyHandlers).find(key => url.includes(key))] : null;
 }
 
-// 
-function removeAdBanner(e) {
-    if (e.data) {
-        if (e.data.close_ad_setting) delete e.data.close_ad_setting;
-        if (e.data.vip_title_blog) delete e.data.vip_title_blog;
-        if (e.data.tips) delete e.data.tips;
+function removeAdBanner(data) {
+    if (data?.data) {
+        delete data.data.close_ad_setting;
+        delete data.data.vip_title_blog;
+        delete data.data.tips;
     }
-    return e;
 }
 
-function modifiedUserCenter(e) {
-    if (e.data && e.data.cards) {
-        e.data.cards = e.data.cards.filter(card => {
-            card.items = card.items.filter(item => 
-                item.type !== "personal_vip" && item.type !== "ic_profile_wallpaper"
-            );
+function modifyUserCenter(data) {
+    if (data?.data?.cards) {
+        data.data.cards = data.data.cards.filter(card => {
+            card.items = card.items.filter(item => !["personal_vip", "ic_profile_wallpaper"].includes(item.type));
             return card.items.length > 0;
         });
     }
-    return e;
 }
 
-function removeTopics(e) {
-    if (e.data) {
-        e.data.order = ["discover", "search_topic"];
-        if (Array.isArray(e.data.discover)) {
-            e.data.discover.splice(0, 1);
-        }
+function removeTrendsCards(data) {
+    if (data?.data) {
+        data.data.order = ["discover", "search_topic"];
+        if (Array.isArray(data.data.discover)) data.data.discover.shift();
     }
-    return e;
 }
 
-function removeIntlOpenAds(e) {
-    if (e.data) {
-        e.data = { display_ad: 1 };
+function removeOpenAds(data) {
+    if (data?.data) {
+        data.data = { display_ad: 1 };
     }
-    return e;
 }
 
-// function removeSearchTopic(e) {
-//     if (e.data && e.data.search_topic && e.data.search_topic.cards.length > 0) {
-//         e.data.search_topic.cards = Object.values(e.data.search_topic.cards).filter(e => e.type !== "searchtop");
-//         if (e.data.trending_topic) delete e.data.trending_topic;
-//     }
-//     return e;
-// }
+let body = $response.body;
+let url = $request.url;
+let modifyHandler = getModifyHandler(url);
 
-
-// 
-var body = $response.body;
-var url = $request.url;
-let modifyFunction = getModifyMethod(url); 
-
-if (modifyFunction) {
-    //console.log(modifyFunction.name);
-    let data = JSON.parse(body.match(/\{.*\}/)[0]); 
-    modifyFunction(data); 
-    body = JSON.stringify(data); 
+if (modifyHandler) {
+    try {
+        let data = JSON.parse(body.match(/\{.*\}/)?.[0] || "{}");
+        modifyHandler(data);
+        body = JSON.stringify(data);
+    } catch (e) {
+        console.log("JSON 解析错误:", e);
+    }
 }
 
 $done({ body });
