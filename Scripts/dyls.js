74d0d6dc5baf97efc07fad6facc930ed4ed6a0a3
mujinbo <316216012@qq.com>
2024-10-30 21:56:32 +0800

    Update dyls.js

--- Scripts/dyls.js
+++ Scripts/dyls.js
@@ -2,7 +2,6 @@ const url = $request.url;
 let obj = JSON.parse($response.body);
 if (url.includes("/movie/index_recommend")) {
     obj.data = obj?.data?.filter(item => item.layout !== "advert_self");
-
     obj.data?.forEach(item => {
         item.list = item.list?.filter(subItem => subItem.type !== 3);
     });
