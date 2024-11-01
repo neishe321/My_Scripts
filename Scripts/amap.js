14ced5b3fde3b37d361f0bd4cb5f3a7eb8e71333
mujinbo <316216012@qq.com>
2024-11-01 17:50:48 +0800

    Update amap.js

--- Scripts/amap.js
+++ Scripts/amap.js
@@ -2,18 +2,16 @@ const obj = JSON.parse($response.body);
 const url = $request.url;
 
 if (url.includes("ws/promotion-web/resource")) {
-	// 打车
+    // 打车
     ["icon", "banner", "tips", "popup", "bubble", "other"].forEach(el => obj.data?.[el] && (obj.data[el] = []));
-} 
-else if (url.includes("profile/index/node")) {
-	// 我的
+} else if (url.includes("profile/index/node")) {
+    // 我的
     delete obj.data?.tipData;
     obj.data?.cardList && (obj.data.cardList = Object.values(obj.data.cardList).filter(
-        a => ["MyOrderCard", "GdRecommendCard"].includes(a.dataType)
-    ));
+        a => ["MyOrderCard", "GdRecommendCard"].includes(a.dataType)));
+} else if (url.includes("search/nearbyrec_smart")) {
+    // 附近
+    if (obj.data?.modules) obj.data.modules = {};
 }
-else if ( - 1 != $request.url.indexOf("search/nearbyrec_smart")){
-	// 附近
-	obj.data.modules && obj.data.modules= {};
-}
+
 $done({ body: JSON.stringify(obj) });
