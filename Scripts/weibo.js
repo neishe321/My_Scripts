3f0a7e0f790367c603089b89737db11a0eacfb81
mujinbo <316216012@qq.com>
2024-11-03 19:48:01 +0800

    Update weibo.js

--- Scripts/weibo.js
+++ Scripts/weibo.js
@@ -10,7 +10,7 @@ try {
     return;
 }
 
-// 过滤数组
+// 数组筛选
 function filterItems(items) {
     return items.filter(item => {
         return !(
@@ -20,22 +20,35 @@ function filterItems(items) {
     });
 }
 
-if (url.includes("comments/build_comments")) {
+
+if (url.includes("/profile/me")) {
+    console.log('处理个人界面');
+    obj.items = obj.items.slice(0, 2);
+    if (obj.items.length > 0 && obj.items[0].header) {
+        delete obj.items[0].header.vipIcon;
+        delete obj.items[0].header.vipView;
+    }
+}
+
+else if (url.includes("comments/build_comments")) {
     // 热门
     if (obj.datas) {
-        console.log("去除帖子评论区推广");
+        console.log("处理评论区推广");
         obj.datas = obj.datas.filter(item => !item.adType);
     }
-} else if (url.includes("guest/statuses_extend")) {
+} 
+
+else if (url.includes("guest/statuses_extend") || url.includes("statuses/extend")) {
     // 热门
-    console.log("去除帖子牛皮癣推广");
+    console.log("处理详情页推广");
     delete obj.head_cards;
     delete obj.trend;
     delete obj.snapshot_share_customize_dic;
     delete obj.dynamic_share_items;
     delete obj.report_data;
-} else if (url.includes("/search/finder")) {
-	// 发现
+}
+
+else if (url.includes("/search/finder")) {
     console.log("处理发现页面");
     const channels = obj.channelInfo?.channels || [];
     channels.forEach(channel => {
@@ -49,20 +62,32 @@ if (url.includes("comments/build_comments")) {
             channel.payload.items = filterItems(channel.payload.items);
         }
     });
-} else if (url.includes("/search/container_timeline") || url.includes("/search/container_discover")) {
+} 
+
+else if (url.includes("/search/container_timeline") || url.includes("/search/container_discover")) {
     console.log("刷新发现页面");
     if (obj?.items) {
         obj.items = filterItems(obj.items);
     }
-} else if (url.includes("/2/searchall?")) {
-	 // 搜索
-    console.log('优化搜索结果');
+} 
+
+else if (url.includes("/2/searchall?")) {
+    console.log('处理搜索结果');
     obj.items = obj.items.filter(item => item.category !== "feed" && item.category !== "cell");
     obj.items.forEach(currentItem => {
         if (currentItem.items) {
             currentItem.items = currentItem.items.filter(subItem => subItem.category !== 'card' && subItem.category !== 'cell');
         }
     });
+}
+
+else if (url.includes("/statuses/container_timeline_hot")) {
+    console.log('处理首页推荐动态推广');
+    if (obj.items) {
+        obj.items = obj.items.filter(item => item.category === "feed");
+        const categoriesToRemove = ["trend", "hot_ad"];
+        obj.items = obj.items.filter(item => !categoriesToRemove.includes(item.item_category));
+    }
 }
 
 $done({ body: JSON.stringify(obj) });
