94a33e55b2daf9c5c115cd5801b6725803e98aed
mujinbo <316216012@qq.com>
2024-11-05 11:41:27 +0800

    Update amap.js

--- Scripts/amap.js
+++ Scripts/amap.js
@@ -1,41 +1,54 @@
 const obj = JSON.parse($response.body);
 const url = $request.url;
 
-const nearbyModules = ["banner", "contentPoster", "feedRec"];
-// const rideModules = ["banner", "other", "bubble", "popup", "push"];
-const taxiModules = ["taxi"];
+// 定义模块
+const moduleActions = {
+    "c3frontend/af-nearby/nearby": {
+        modules: ["banner", "contentPoster", "feedRec"],
+        action: (obj, modules) => {
+            modules?.forEach(key => delete obj.data?.modules?.[key]);
+        }
+    },
+    "ws/promotion-web/resource": {
+        action: (obj) => {
+            obj.data = {}; 
+        }
+    },
+    "ws/shield/frogserver/aocs/updatable": {
+        modules: ["taxi"],
+        action: (obj, modules) => {
+            modules?.forEach(key => delete obj.data?.[key]);
+        }
+    },
+    "profile/index/node": {
+        action: (obj) => {
+            delete obj.data?.tipData;
+            if (Array.isArray(obj.data?.cardList)) {
+                obj.data.cardList = Object.values(obj.data.cardList).filter(a => 
+                    ["MyOrderCard", "GdRecommendCard"].includes(a.dataType)
+                );
+            }
+        }
+    },
+    "valueadded/alimama/splash_screen": {
+        action: (obj) => {
+            if (Array.isArray(obj.data?.ad)) {
+                obj.data.ad.forEach(ad => {
+                    ad.set.setting.display_time = 0;
+                    ad.creative[0].start_time = 2240150400;
+                    ad.creative[0].end_time = 2240150400;
+                });
+            }
+        }
+    }
+};
 
-if (url.includes("c3frontend/af-nearby/nearby")) {
-    // 附近
-    nearbyModules.forEach(key => delete obj.data?.modules?.[key]);
-} 
-else if (url.includes("ws/promotion-web/resource")) {
-    // 打车
-    // rideModules.forEach(key => delete obj.data?.[key]);
-    obj.data = {};
-} 
-else if (url.includes("ws/shield/frogserver/aocs/updatable")) {
-    // 打车卡片
-    taxiModules.forEach(key => delete obj.data?.[key]);
-} 
-else if (url.includes("profile/index/node")) {
-    // 我的
-    delete obj.data?.tipData; 
-    if (Array.isArray(obj.data?.cardList)) {
-        obj.data.cardList = Object.values(obj.data.cardList).filter(a => 
-            ["MyOrderCard", "GdRecommendCard"].includes(a.dataType)
-        );
+// 执行对应的操作
+Object.keys(moduleActions).forEach(key => {
+    if (url.includes(key)) {
+        const action = moduleActions[key];
+        action.action(obj, action.modules); // 直接传递模块数组或 obj
     }
-} 
-else if (url.includes("valueadded/alimama/splash_screen")) {
-    // 开屏
-    if (Array.isArray(obj.data?.ad)) {
-        obj.data.ad.forEach(ad => {
-            ad.set.setting.display_time = 0;
-            ad.creative[0].start_time = 2240150400;
-            ad.creative[0].end_time = 2240150400;
-        });
-    }
-}
+});
 
 $done({ body: JSON.stringify(obj) });
