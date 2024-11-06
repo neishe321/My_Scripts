5dcaa710b02b4625278c8694bdc0d89a9e737983
mujinbo <316216012@qq.com>
2024-11-06 19:29:18 +0800

    Update weibo.js

--- Scripts/weibo.js
+++ Scripts/weibo.js
@@ -76,8 +76,8 @@ function RemoveCardtype(array = []) {
             (item?.itemId && hot_card_keywords.some(keyword => item?.itemId.includes(keyword))) ||
             (item?.data?.wboxParam) || 			// wboxParam
             (item?.data?.cate_id === "1114") ||   	// wboxParam.png
-	    (items?.data?.card_ad_style === '1') ||  	// 实时图片推广
-	    (items?.data?.card_id === "search_card") || // 实时搜索框文本
+	    (item?.data?.card_ad_style === '1') ||  	// 实时图片推广
+	    (item?.data?.card_id === "search_card") || // 实时搜索框文本
 	   // 下边属于超话卡片
 	    (item?.data?.itemid && hot_card_keywords.some(keyword => item?.data.itemid.includes(keyword)) && item?.data?.itemid !== "sg_bottom_tab_search_input") ||
 	    (item?.data?.header?.title === "绝美壁纸上新")
