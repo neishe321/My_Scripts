5cc1ac80be949d1d11fece2768bab332ee2e011b
mujinbo <316216012@qq.com>
2024-11-08 15:41:48 +0800

    Delete QuantumultX/Scripts directory

--- QuantumultX/Scripts/dyls.js
+++
@@ -1,9 +0,0 @@
-let obj = JSON.parse($response.body);
-
-if ($request.url.indexOf("/movie/index_recommend") !== -1) {
-    obj.data = obj?.data?.filter(item => item.layout !== "advert_self");
-    obj.data?.forEach(item => {
-        item.list = item.list?.filter(subItem => subItem.type !== 3);
-    });
-}
-$done({ body: JSON.stringify(obj) });
