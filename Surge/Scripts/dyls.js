a908c575bb8d28729c29facf84328737792a0325
mujinbo <316216012@qq.com>
2024-11-08 13:30:00 +0800

    Update dyls.js

--- Surge/Scripts/dyls.js
+++ Surge/Scripts/dyls.js
@@ -1,6 +1,6 @@
 let obj = JSON.parse($response.body);
 if (Array.isArray(obj?.data)) {
-    console.long("表哥 我出来了哦")
+    console.long("表哥 我出来了哦");
     obj.data = obj.data.filter(item => item.layout !== "advert_self");
     obj.data.forEach(item => {
         if (Array.isArray(item.list)) {
